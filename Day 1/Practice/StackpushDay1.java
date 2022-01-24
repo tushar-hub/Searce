3828fb4512db103bd4588a20379bbae3a4e5f267
tushar-hub <62297423+tushar-hub@users.noreply.github.com>
2022-01-24 17:00:39 +0530

    Add files via upload

---
+++ Day 1/Practice/StackpushDay1.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+public class StackpushDay1 {
+public static void main(String[] args)
+{
+// Create an empty stack that contains String objects.
+   Stack<String> st = new Stack<>();
+
+// Checks that stack is empty or not.
+   boolean empty = st.empty();
+   System.out.println("Is stack empty: " +empty);
+
+// Adds elements to the top of stack using push() method.
+   st.push("Sunday");
+   st.push("Monday");
+   st.push("Tuesday");
+   st.push("Wednesday");
+   st.push("Thursday");
+   st.push("Friday");
+   st.push("Saturday");
+
+// Displaying elements from the stack.
+   System.out.println("Elements of Stack: " +st);
+ }
+}
